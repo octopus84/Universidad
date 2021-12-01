d0bbf33d28d8db75035ad1d356638efad946c792
octopus84 <octaviogodoyg@gmail.com>
2021-12-01 19:00:16 -0300

    Se crea modelo

--- Aplicaciones/Academico/models.py
+++ Aplicaciones/Academico/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.db.models.fields import CharField
 
 # Create your models here.
+
+class Curso(models.Model):
+    codigo=models-CharField(primary_key=True, max_length=6)
+    nombre= models.CharField(max_length=50)
+    creditos= models.PositiveSmallIntegerField()
+    
